c7165e33595843186bf2593a59862ca6bd046128
PF <pierre-francois.gimenez@irit.fr>
2017-08-10 11:43:04 +0200

    Dependencies as submodules

--- high_level/update.sh
+++ high_level/update.sh
@@ -1,42 +1,4 @@
-cd ../..
-
-cd dependency-injector
+git submodule update --recursive
 git pull
-rm *.jar
-ant clean
-ant
-cd ..
-
-cd config/core
-git pull
-rm *.jar
-ant clean
-ant
-cd ../..
-
-cd graphic-toolbox/core
-git pull
-cp ../../config/core/config.jar lib/
-rm *.jar
-ant clean
-ant
-cd ../..
-
-cd The-Kraken-Pathfinding/core
-git pull
-cp ../../graphic-toolbox/core/graphic.jar lib/
-cp ../../config/core/config.jar lib/
-cp ../../dependency-injector/injector.jar lib/
-rm *.jar
-ant clean
-ant
-cd ../..
-
-cd eurobotruck/high_level
-git pull
-cp ../../graphic-toolbox/core/graphic.jar lib/
-cp ../../config/core/config.jar lib/
-cp ../../dependency-injector/injector.jar lib/
-cp ../../The-Kraken-Pathfinding/core/kraken.jar lib/
 ant clean
 ant
