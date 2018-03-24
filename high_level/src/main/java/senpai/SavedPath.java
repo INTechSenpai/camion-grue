309770db11108a6199de3e3df83a877dbb3a2c2a
PF <pierre-francois.gimenez@laposte.net>
2018-03-24 20:20:34 +0100

    Structure sérialisable

--- high_level/src/main/java/senpai/SavedPath.java
+++ high_level/src/main/java/senpai/SavedPath.java
@@ -1,11 +1,13 @@
 package senpai;
 
+import java.io.Serializable;
 import java.util.List;
 import pfg.kraken.robot.ItineraryPoint;
 import pfg.kraken.utils.XYO;
 
-public class SavedPath
+public class SavedPath implements Serializable
 {
+	private static final long serialVersionUID = 1L;
 	public List<ItineraryPoint> path;
 	public XYO depart;
 	
