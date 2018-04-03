62fbfe7331cd1ed9014b92675cee1d4caa681f63
PF <pierre-francois.gimenez@laposte.net>
2018-04-03 22:32:44 +0200

    Bugfix script GPIO

--- high_level/pi/gpio_halt.sh
+++ high_level/pi/gpio_halt.sh
@@ -7,10 +7,10 @@ gpio write 22 0 # diode éteinte par défaut
 # on s'arrête si la pin passe à l'état bas
 while [ $(gpio read 21) -eq 1 ]
 do
-    sleep 0.2
+    sleep 0.5
 done
 wall "Arrêt du système par GPIO !"
-halt
+sudo halt
 
 #La pin 29 (GPIO 05), wiring pi 21, est reliée à l'interrupteur
 #il y a un pull up, donc tu liras LOW quand on appuie sur le bouton
