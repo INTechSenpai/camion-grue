248090f3521e14de9cf4bad9f7205e8fea2284a7
Sylvain Gaultier <sylvain.gaultier@actronika.com>
2018-04-05 11:18:52 +0200

    LL internal com WIP

--- low_level/lowlevel_master/SlaveCommunication/InternalCom.h
+++ low_level/lowlevel_master/SlaveCommunication/InternalCom.h
@@ -67,9 +67,14 @@ public:
         payload.push_back(byte);
     }
 
-    std::vector<uint8_t> getPayload() const
+    size_t size() const
     {
-        return payload;
+        return payload.size();
+    }
+
+    uint8_t at(size_t i) const
+    {
+        return payload.at(i);
     }
 
     bool payloadIsFull() const
@@ -79,7 +84,7 @@ public:
 
     void setChecksum(uint8_t checksum)
     {
-        uint8_t computedChecksum = computeChecksum();
+        uint8_t computedChecksum = getChecksum();
         valid = checksum == computedChecksum && length == payload.size();
     }
 
@@ -96,12 +101,11 @@ public:
             {
                 output.push_back(payload.at(i));
             }
-            output.push_back(computeChecksum());
+            output.push_back(getChecksum());
         }
     }
 
-private:
-    uint8_t computeChecksum()
+    uint8_t getChecksum() const
     {
         uint8_t computedChecksum = instruction + length;
         for (size_t i = 0; i < payload.size(); i++)
@@ -112,6 +116,7 @@ private:
         return computedChecksum;
     }
 
+private:
     bool valid;
     uint8_t instruction;
     uint8_t length;
@@ -131,32 +136,95 @@ public:
 
     void listen()
     {
+        if (message.isValid())
+        {
+            return;
+        }
         uint32_t beginTime = millis();
         while (serial.available() && millis() - beginTime < INTERNAL_COM_MAX_LISTEN_DURATION)
         {
             uint8_t recByte = serial.read();
+            switch (readPhase)
+            {
+                case HEADER1:
+                    if (recByte == 0xFF) {
+                        readPhase = HEADER2;
+                    } else {
+                        // err
+                    }
+                    break;
+                case HEADER2:
+                    if (recByte == 0xFF) {
+                        readPhase = INSTRUCTION;
+                    } else {
+                        readPhase = HEADER1;
+                        // err
+                    }
+                    break;
+                case INSTRUCTION:
+                    message.setInstruction(recByte);
+                    readPhase = LENGTH;
+                    break;
+                case LENGTH:
+                    message.setLength(recByte);
+                    readPhase = PAYLOAD;
+                    break;
+                case PAYLOAD:
+                    if (message.payloadIsFull()) {
+                        message.setChecksum(recByte);
+                        readPhase = HEADER1;
+                        if (message.isValid()) {
+                            return;
+                        } else {
+                            message.reset();
+                            // err
+                        }
+                    } else {
+                        message.appendToPayload(recByte);
+                    }
+                    break;
+                case default:
+                    break;
+            }
         }
     }
 
-    bool available()
+    bool available() const
     {
-
+        return message.isValid();
     }
 
     InternalMessage getMessage()
     {
-
+        InternalMessage ret = message;
+        message.reset();
+        return ret;
     }
 
-    void sendMessage(InternalMessage m)
+    void sendMessage(const InternalMessage &m)
     {
-
+        if (m.isValid())
+        {
+            serial.write(0xFF);
+            serial.write(0xFF);
+            serial.write(m.getInstruction());
+            serial.write(m.size());
+            for (size_t i = 0; i < m.size(); i++)
+            {
+                serial.write(m.at(i));
+            }
+            serial.write(m.getChecksum());
+        }
+        else
+        {
+            // err
+        }
     }
 
 private:
     enum ReadPhase
     {
-        HEADER1, HEADER2, INSTRUCTION, LENGTH, PAYLOAD, CHECKSUM
+        HEADER1, HEADER2, INSTRUCTION, LENGTH, PAYLOAD
     };
 
     HardwareSerial & serial;
