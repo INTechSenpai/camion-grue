e17df68fb1c72178762468e8c2339031566a7948
PF <pierre-francois.gimenez@irit.fr>
2018-05-07 17:07:44 +0200

    Bugfix test

--- high_level/src/test/java/senpai/Test_Robot.java
+++ high_level/src/test/java/senpai/Test_Robot.java
@@ -70,8 +70,8 @@ public class Test_Robot extends JUnit_Test
 				if(couleur != CubeColor.GOLDEN)
 					for(CubeFace face : CubeFace.values())
 					{
-						new ScriptPriseCube(log, robot, table, cp, null, croix, couleur, face, true);
-						new ScriptPriseCube(log, robot, table, cp, null, croix, couleur, face, false);
+						new ScriptPriseCube(log, robot, table, cp, null, croix, couleur, face, true, false);
+						new ScriptPriseCube(log, robot, table, cp, null, croix, couleur, face, false, false);
 					}
 	}
 	
